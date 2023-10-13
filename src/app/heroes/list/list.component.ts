dfc6e4670608c18244a6694f5ab817632df8e406
fernalp <almanzafernando@hotmail.com>
2023-10-13 13:07:42 -0500

    Finishing section 4

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
 export class ListComponent {
 
   public heroNames: string[] = ["Spiderman", "Ironman", "Hulk", "Thor"];
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop(); // Remove the last item in array
+  }
 
 }
