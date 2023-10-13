7089de2d83f3dfffb1c23e15b4d03b6f0801da33
fernalp <almanzafernando@hotmail.com>
2023-10-12 22:25:30 -0500

    Create Branch 02-bases and progress section 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title = 'Mi Primera App de Angular';
+  public counter: number = 10;
+
+  increaseBy(value: number = 1): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
