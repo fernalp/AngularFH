563570984e91b71532af3a3cf21194d804b554b4
fernalp <almanzafernando@hotmail.com>
2023-10-17 12:26:51 -0500

    Finishing section 5

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      id: "",
+      name: "Trunks",
+      power: 10
+    }
+  ];
+
+  @Output()
+  public onDeleteCharacterById: EventEmitter<string> = new EventEmitter();
+
+
+
+  onDeleteCharacter(id: string): void {
+    // TODO: Emitir el ID del personaje
+    this.onDeleteCharacterById.emit(id);
+  }
+
+
+}
