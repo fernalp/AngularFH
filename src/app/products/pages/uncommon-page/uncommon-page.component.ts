02a5f8c2a71d3c244fbe5cecce0aef023918210a
fernalp <almanzafernando@hotmail.com>
2023-11-09 11:37:49 -0500

    finished page basic and uncommon

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./uncommon-page.component.css']
 })
 export class UncommonPageComponent {
+
+  // i18n Select
+  public name: string = "Fernando";
+  public gender: 'male' | 'female' = "male";
+  public invitationMap = {
+    male: "invitarlo",
+    female: "invitarla",
+  }
+
+  changeClient(): void {
+    this.name = "Julissa";
+    this.gender = "female";
+  }
+
+  // i18n Plural
+
+  public clients: string[] = [
+    "María",
+    "José",
+    "Lucas",
+    "Laura",
+    "Santiago",
+    "Ana",
+    "Mateo",
+    "Nicolás",
+    // "Cristina",
+    // "Felipe",
+    // "Gabriel",
+    // "Rebeca",
+    // "Diego",
+    // "Carolina",
+    // "Andrés",
+  ]
+  public clientsMap = {
+    '=0': "no tenemos ningún cliente esperando.",
+    '=1': "tenemos un cliente esperando.",
+    'other': "tenemos # clientes esperando.",
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //KeyValue - Json
+
+  public person = {
+    name: "Fernando",
+    age: 25,
+    address: "Barranquilla, Atlántico",
+  }
+
+  //Async
+
+  public myObservableTimer: Observable<number> = interval(2000);
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Tenemos data en la promesa")
+    }, 1500)
+  })
 
 }
