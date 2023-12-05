218b995dd9871dd684834cc9b193490957afd2d4
fernalp <almanzafernando@hotmail.com>
2023-12-04 21:52:38 -0500

    Finished Section 19 - LifeCycle of Angular

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductComponent } from './pages/product/product.component';
+import { PriceComponent } from './components/price/price.component';
+
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    PriceComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule
+  ],
+  exports: [
+    ProductComponent
+  ]
+})
+export class ProductsModule { }
